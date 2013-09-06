9cb84caa2a4f80a2382169e8bbf70e30ffc922e6
TriNguyenH <tringuyenH@gmail.com>
2013-09-05 17:01:12 -0700

    update color

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 $(function() {
+
+    getCurrentSession();
+
     $('#btnCalculate').on("click", function(e) {
 
         $subtotal = 0;
@@ -18,7 +21,57 @@ $(function() {
 
             $("#span18").html("$" + Math.round(($subtotal * 1.18) / $num_people * 100) / 100);
 
-            $(".result").css("display","block");
+            $(".result").css("display", "block");
         }
     });
 });
+
+function getCurrentSession() {
+    $hasCurrentSession = true;
+    
+    //Get current session
+    if ($hasCurrentSession == true) {
+
+        $transaction = $("#transaction");
+        $transaction.addClass("active");
+        $transaction.html("Session started on 1/1/1900 12:00 PM");
+        $transaction.show();
+        
+        $transaction = $("#li_session");
+
+        $transaction.removeClass('add');
+
+        $transaction.addClass('close');
+        $transaction.addClass('stop');
+
+        $transaction.find("a").first().text("Stop Current Session");
+    }
+}
+
+function addFund() {
+    alert("Fund added");
+}
+
+function startSession() {
+
+}
+
+function stopSession() {
+
+}
+
+function displayBalance() {
+
+}
+
+function displayStatistics() {
+
+}
+
+function displaySessions() {
+
+}
+
+function displayTransactions() {
+
+}
